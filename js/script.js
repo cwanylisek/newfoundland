ae8a0011cdfe5324607eee3a019069748765055d
Bartek Tylkowski <30731599+cwanylisek@users.noreply.github.com>
2018-01-19 16:40:08 +0100

    no_rwd

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function myMap() {
-var mapOptions = {
-    center: new google.maps.LatLng(52.137049, 20.153432),
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-}
+    var mapOptions = {
+        center: new google.maps.LatLng(52.406374, 16.925168100000064),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
